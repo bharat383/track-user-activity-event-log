0d981d7ef35518676976614401e6975c37ea3167
Bharat Parmar <bharatparmar383@gmail.com>
2016-04-25 19:13:09 +0530

    Update trackuser.php

--- ajax/trackuser.php
+++ ajax/trackuser.php
@@ -1,9 +1,8 @@
 <?php
 
-class TrackUser 
-{
-	function SaveScreenshot()
-	{
+class TrackUser{
+	function SaveScreenshot(){
+
 		$event_log_array = array(
 				'click'=>'User has clicked on the Web Page',
 				'form-submit'=>'User has submitted Form Submit.',
@@ -12,22 +11,23 @@ class TrackUser
 				'right-click'=>'User has clicked Right Button of the Mouse.',
 				'copy'=>'User has copied web page content.',
 			);
+			
 		$filteredData=substr($_POST['image_code'], strpos($_POST['image_code'], ",")+1);
 
 		//Decode the string
 		$unencodedData=base64_decode($filteredData);
 
 		//Save the image
-		if(!file_exists("../screenshot/")) {mkdir("../screenshot");}
+		if(!file_exists("../screenshot/")) {
+			mkdir("../screenshot");
+		}
 		$file_path="../screenshot/";
 
 		$filename = date("Y-m-d-H-i-s").".png";
 
-		//array_map('unlink', glob($file_path."/*.png"));
 		file_put_contents($file_path."/".$filename, $unencodedData);
 
 		//SAVE EVENT LOG
-		//if(!file_exists("../screenshot/event-log.log")) {mkdir("../screenshot/event-log.log");}
 		$file_content = @file_get_contents("../screenshot/event-log.log");
 		$file_content.="\n ".$event_log_array[$_POST['event_name']].". Image : ".$filename.". Time : ".date("Y-m-d-H-i-s").". IP Address : ".$_SERVER['REMOTE_ADDR'];
 
